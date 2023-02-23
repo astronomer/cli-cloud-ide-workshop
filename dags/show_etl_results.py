2dbf208e3b484b26e7688238644755025ee08d02
Kenten Danas <kentdanas@gmail.com>
2023-02-23 11:03:03 -0800

    Fix duckdb import issue and update results DAG

--- dags/show_etl_results.py
+++ dags/show_etl_results.py
@@ -8,7 +8,9 @@ from airflow.decorators import dag, task
 def get_results():
     cursor = duckdb.connect("etl_example")
     results = cursor.execute("SELECT * FROM monthly_avg_area_burned").fetchall()
+    max_month = cursor.execute("SELECT month FROM monthly_avg_area_burned ORDER BY area DESC LIMIT 1;").fetchall()
     print(results)
+    return results, max_month
 
 
 @dag(schedule=None, start_date=datetime(2023, 2, 1), catchup=False)
